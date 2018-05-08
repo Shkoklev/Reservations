2db27f24286e011c0d4cecaee6645cae2e567e2d
Dimitri <Dimitri.Shkoklev@hotmail.com>
2018-05-08 12:46:35 +0200

    another update

--- reservations-client/src/app/pages/register/register.component.ts
+++ reservations-client/src/app/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../../models/User';
 
 @Component({
   selector: 'app-register',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+
+  firstName: String;
+  lastName: String;
+  email: String;
+  username: String;
+  password: String;
 
   constructor() { }
 
